02e670e3a281de6db24a8b95d081ec94fa305bda
ShineCheng <afireflytw@gmail.com>
2019-05-21 18:40:28 +0800

    start

---
+++ practice/vue/router/data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+        title: "article 1 title",
+        article: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet mollis elit, feugiat bibendum libero. Integer et cursus ex. Sed sodales pharetra nunc, eget consectetur massa pulvinar sit amet. Proin maximus velit non erat egestas, in ornare elit tincidunt. Nunc augue mauris, efficitur euismod velit ac, blandit suscipit orci. Morbi finibus risus consectetur eros placerat, nec luctus ex pellentesque. Suspendisse potenti. Mauris in urna a enim egestas semper. Donec tincidunt, purus id pulvinar efficitur, est leo porta nunc, eu molestie lacus ante vel urna. Sed vestibulum erat a sapien aliquam pellentesque."
+    },
+    {
+        title: "article 2 title",
+        article: "Nam rhoncus, nisi rhoncus imperdiet rhoncus, neque lacus molestie nibh, vel tincidunt mauris turpis nec orci. In nec euismod arcu, eu malesuada justo. Mauris nec tempus ligula, vulputate laoreet sem. Mauris dignissim quam vel neque vulputate scelerisque. Ut sed nibh ut sem pellentesque malesuada eget et orci. Donec rutrum, massa quis rutrum feugiat, ex odio varius neque, in posuere justo lectus eget orci. Quisque non enim nulla. Proin tempor aliquet tellus a porta. Mauris cursus ultricies lobortis. Fusce at felis eget turpis finibus interdum ac at turpis. Fusce velit justo, tincidunt non diam et, ornare tempor eros. Phasellus at consequat nibh, quis consectetur arcu. Cras facilisis et felis quis mattis."
+    }
+]
