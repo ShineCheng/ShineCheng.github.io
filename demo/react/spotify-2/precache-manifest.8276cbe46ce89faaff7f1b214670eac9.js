7422f6d374849b981ab49f102d563570ec37c48a
ShineCheng <afireflytw@gmail.com>
2019-07-26 14:23:45 +0800

    test

---
+++ demo/react/spotify-2/precache-manifest.8276cbe46ce89faaff7f1b214670eac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2635c2747bbce99e3d1dac02ef172e43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca0422d086fd2cc0202f",
+    "url": "/static/css/main.5f6367b1.chunk.css"
+  },
+  {
+    "revision": "69fb722541f8023563ea",
+    "url": "/static/js/2.cb28cbde.chunk.js"
+  },
+  {
+    "revision": "ca0422d086fd2cc0202f",
+    "url": "/static/js/main.2b112071.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "231419b67661de032d4efdac7c47f148",
+    "url": "/static/media/anti.231419b6.jpg"
+  }
+]);
---
+++ demo/react/spotify-2/precache-manifest.a57829104b7a52b51c1304db09a104b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02e69e45d6b33b5f368a02d18166491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6baf998b252d8459bf6c",
+    "url": "/static/css/main.4cdb5e57.chunk.css"
+  },
+  {
+    "revision": "69fb722541f8023563ea",
+    "url": "/static/js/2.cb28cbde.chunk.js"
+  },
+  {
+    "revision": "6baf998b252d8459bf6c",
+    "url": "/static/js/main.2b06a6da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
